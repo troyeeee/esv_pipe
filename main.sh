29e7a6d654cd457ac7f471529104e2cad6c86b4f
guangze <guangzep@gmail.com>
2022-01-30 13:19:02 +0800

    init

---
+++ main.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+SVABA=svaba
+CONFIGMANTA=configManta.py
+SAMTOOLS=samtools
+LUMPY_EXPRESS=/home/grads/gzpan2/apps/lumpy-sv/bin/lumpyexpress
+SVTYPER=/home/grads/gzpan2/apps/miniconda3/envs/cityu2/bin/svtyper
+SCRIPTS=/public/panguangze/surv/reads_sv/pipeline/scripts
+SURVIVOR=/home/grads/gzpan2/apps/SURVIVOR/Debug/SURVIVOR
+EXTRACT_HAIR = /home/grads/gzpan2/clion/extractHairs/build/ExtractHAIRs
+#ESplitReads_BwaMem=/home/grads/gzpan2/apps/lumpy-sv//scripts/extractSplitReads_BwaMem
+#GRIDSS=/home/grads/gzpan2/apps/miniconda3/envs/cityu2/share/gridss-2.8.0-0/gridss.jar
+bam=$1
+ref=$2
+out_dir=$3
+threads=$4
+if [ ! -d $out_dir ]; then
+  mkdir $out_dir
+fi
+if [ ! -d $out_dir/svaba ]; then
+  mkdir $out_dir/svaba
+fi
+if [ ! -d $out_dir/manta ]; then
+  mkdir $out_dir/manta
+fi
+if [ ! -d $out_dir/lumpy ]; then
+  mkdir $out_dir/lumpy
+fi
+if [ ! -d $out_dir/gridss ]; then
+  mkdir $out_dir/gridss
+fi
+# svaba
+$SVABA run -t $bam -G $ref -a $out_dir/svaba/svaba --read-tracking --germline -p $threads
+$SVTYPER -B $bam -i $out_dir/svaba/svaba.svaba.sv.vcf > $out_dir/svaba/svaba.svtyper.sv.vcf
+python $SCRIPTS/adjust_svtyper_genotypes.py $out_dir/svaba/svaba.svtyper.sv.vcf > $out_dir/svaba/svaba.adjusted.vcf
+# manta
+$CONFIGMANTA --bam $bam --referenceFasta $ref --runDir $out_dir/manta --generateEvidenceBam
+$out_dir/manta/runWorkflow.py -m local -j $threads -g 100
+$SVTYPER -B $bam -i $out_dir/manta/results/variants/diploidSV.vcf> $out_dir/manta/manta.svtyper.vcf
+pythoh $SCRIPTS/parser_reads.py -v $out_dir/manta/manta.svtyper.vcf -b $out_dir/manta/results/evidence/evidence_0.test.s.ngs.bam -o $out_dir/manta/manta.evidence.vcf
+python $SCRIPTS/adjust_svtyper_genotypes.py $out_dir/manta/manta.evidence.vcf > $out_dir/manta/manta.adjusted.vcf
+# lumpy
+# $SAMTOOLS view -uF 0x0002 $bam | $SAMTOOLS view -uF 0x100 - | $SAMTOOLS view -uF 0x0004 - | $SAMTOOLS view -uF 0x0008 - | $SAMTOOLS view -bF 0x0400 - | $SAMTOOLS sort - -o $out_dir/lumpy/lumpy.discordant.sort.bam
+# $SAMTOOLS view -h $bam | $ESplitReads_BwaMem -i stdin | $SAMTOOLS view -Sb - | $SAMTOOLS sort - -o $out_dir/lumpy/lumpy.sr.sort.bam
+# $LUMPY_EXPRESS -B $bam -S $out_dir/lumpy/lumpy.sr.sort.bam -D $out_dir/lumpy/lumpy.discordant.sort.bam -o $out_dir/lumpy/lumpy.vcf
+# $SVTYPER -B $bam -i $out_dir/lumpy/lumpy.vcf > $out_dir/lumpy/lumpy.svtyper.vcf
+# pythoh $SCRIPTS/parser_reads.py -v $out_dir/lumpy/lumpy.svtyper.vcf -b $out_dir/lumpy/results/evidence/evidence_0.test.s.ngs.bam -o $out_dir/lumpy/lumpy.evidence.vcf
+
+# python $SCRIPTS/adjust_svtyper_genotypes.py $out_dir/lumpy/lumpy.evidence.vcf > $out_dir/lumpy/lumpy.adjusted.vcf
+
+# generate input for survivor
+touch $out_dir/sur.input
+# echo "$out_dir/lumpy/lumpy.adjusted.vcf" >> $out_dir/sur.input
+echo "$out_dir/svaba/svaba.adjusted.vcf" >> $out_dir/sur.input
+echo "$out_dir/lumpy/manta.adjusted.vcf" >> $out_dir/sur.input
+#sur
+$SURVIVOR merge $out_dir/survivor_inputs 1000 1 1 0 0 10 $out_dir/survivor.output.vcf
+bcftools sort $out_dir/survivor.output.vcf -o $out_dir/survivor.sort.vcf
+python $SCRIPTS/combine_combined.py $out_dir/survivor_sorted.vcf SUR_SAMPLE $out_dir/survivor_inputs $SCRIPTS/all.phred.txt > $out_dir/combined.genotyped.vcf
+bgzip $out_dir/combined.genotyped.vcf
+tabix $out_dir/combined.genotyped.vcf.gz
+$EXTRACT_HAIR --bam $bam --vcf $out_dir/combined.genotyped.vcf.gz --out $out_dir/ext.lst --breakends 1 --mate_at_same 1 --support_read_tag READNAMES
+# gridss
+#java -cp $GRIDSS gridss.CallVariants -r $ref O=$out_dir/gridss/gridss.vcf.gz --threads $threads $bam
+
+#GRIDSS_JAR=/home/grads/gzpan2/apps/miniconda3/envs/cityu2/share/gridss-2.8.0-0/gridss.jar
+#java -ea -Xmx31g \
+##-Dsamjdk.create_index=true \
+#-Dsamjdk.use_async_io_read_samtools=true \
+#-Dsamjdk.use_async_io_write_samtools=true \
+#-Dsamjdk.use_async_io_write_tribble=true \
+#-Dgridss.gridss.output_to_temp_file=true \
+#-cp $GRIDSS_JAR gridss.CallVariants \
+#TMP_DIR=./tmp \
+#WORKING_DIR=. \
+#REFERENCE_SEQUENCE="$ref" \
+#INPUT="$bam" \
+#OUTPUT="$out_dir/grids/grids.vcf.gz" \
+#ASSEMBLY="$out_dir/grids/grids.assembly.bam" \
+#THREADS=$threads \
+#2>&1 | tee -a gridss.$HOSTNAME.$$.log
